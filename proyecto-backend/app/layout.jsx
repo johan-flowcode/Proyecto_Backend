6725eb3f9ed9ad91cb961d029891b0e21b2062ec
johan-flowcode <moises.escudero.os@gmail.com>
2024-02-28 13:13:24 +0100

    creacion carpeta mas archivos proyecto backend

---
+++ proyecto-backend/app/layout.jsx
@@ -0,0 +1,21 @@
+import "@/styles/globals.css";
+//import Provider from "@components/Provider";
+
+export const metadata = {
+  title: "Artify",
+  description: "Discover and Share Art",
+};
+
+const layout = ({ children }) => {
+  return (
+    <html lang="en">
+      <body>
+       
+          <main>{children}</main>
+       
+      </body>
+    </html>
+  );
+};
+
+export default layout;
